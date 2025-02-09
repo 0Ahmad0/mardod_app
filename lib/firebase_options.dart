6a5b06dfe78f11d4b30db8281cfea45a4c6ba4e3
Ahmad Mriwed <mr.ahmadmriwed@gmail.com>
2025-02-09 13:17:33 +0300

    change firebase

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -19,7 +19,7 @@ class DefaultFirebaseOptions {
     if (kIsWeb) {
       throw UnsupportedError(
         'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
+            'you can reconfigure this by running the FlutterFire CLI again.',
       );
     }
     switch (defaultTargetPlatform) {
@@ -28,22 +28,22 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+              'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.macOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+              'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+              'you can reconfigure this by running the FlutterFire CLI again.',
         );
       case TargetPlatform.linux:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
+              'you can reconfigure this by running the FlutterFire CLI again.',
         );
       default:
         throw UnsupportedError(
@@ -53,10 +53,10 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCoGWStpUighrPs0y-Ty61v2LZ4rabbL5M',
-    appId: '1:914393860689:android:4a5b6d5260ecc17dc04670',
-    messagingSenderId: '914393860689',
-    projectId: 'mardod-app-43884',
-    storageBucket: 'mardod-app-43884.firebasestorage.app',
+    apiKey: 'AIzaSyB_ju9y5fdawJQpPCHE1Jqf8t4tnbJ_ziE',
+    appId: '1:605154990340:android:d95923f01cc55aa3725851',
+    messagingSenderId: '605154990340',
+    projectId: 'mardod-storage',
+    storageBucket: 'mardod-storage.firebasestorage.app',
   );
 }
